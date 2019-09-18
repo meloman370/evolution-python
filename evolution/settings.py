b85e0840ed75235fc8b546cacacff802be2365c4
Dima Yankouski <meloman370@gmail.com>
2019-09-18 18:39:16 +0300

    allowed host

--- evolution/settings.py
+++ evolution/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u@=hgiu*pxc&#cgp8jf68i8=)i6oo5a=%k#&fk&q1#_!hh8r(+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stormy-river-20369.herokuapp.com']
 
 
 # Application definition
